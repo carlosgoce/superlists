c16ad107832737b49c445f366ccb6445549293ad
Carlos González Goce <carlosgoce@gmail.com>
2014-10-13 14:31:13 +0200

    Fixed bug

--- superlistsTdd/urls.py
+++ superlistsTdd/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
